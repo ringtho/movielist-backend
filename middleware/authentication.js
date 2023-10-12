83b235b6269af9d1de86dc4d810d4c63ae09b8f3
Smith Ringtho <sringtho@gmail.com>
2023-10-12 10:41:24 +0300

    Feat: Added Authentication middleware to check for authentication

---
+++ middleware/authentication.js
@@ -0,0 +1,20 @@
+const { UnAuthorizedError } = require("../errors")
+const jwt = require('jsonwebtoken')
+
+const authenticateUser = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new UnAuthorizedError('Authentication Invalid')
+    }
+    const token = authHeader.split(" ")[1]
+    console.log(token)
+    try {
+        const { email, id } = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = { email , id }
+        next()
+    } catch (error) {
+        throw new UnAuthorizedError('Authentication Invalid')
+    }
+}
+
+module.exports = authenticateUser
