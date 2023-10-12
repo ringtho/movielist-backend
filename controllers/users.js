f79f02cc34eb7573057b8417414f20ea175b3946
Smith Ringtho <sringtho@gmail.com>
2023-10-12 12:15:14 +0300

    Feat: Added update user by name

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes")
 const User = require("../models/user")
+const { BadRequestError, NotFoundError } = require("../errors")
 
 const getUser = async (req, res) => {
     const { id } = req.user
@@ -10,6 +11,33 @@ const getUser = async (req, res) => {
     res.status(StatusCodes.OK).json({ user, success: true})
 }
 
+const updateUser = async (req, res) => {
+    const { id } = req.user
+    const { name, profileImg } = req.body
+    if (JSON.stringify(req.body) === '{}') {
+        throw new BadRequestError('Please provide a name or profile image')
+    }
+    const data = {}
+    if (name) {
+        data.name = name
+    }
+    if (profileImg) {
+        data.profileImg = profileImg
+    }
+    const [rowCount] = await User.update(data, {
+        where: { id }, 
+        returning: true 
+    })
+    if (rowCount === 0) {
+        throw new NotFoundError('User not Found')
+    }
+    const updatedUser = await User.findOne({ 
+        where: { id }, attributes: { exclude: ['password', 'id']}
+    })
+    res.status(StatusCodes.OK).json({ user: updatedUser, success: true })
+}
+
 module.exports = {
-    getUser
+    getUser,
+    updateUser
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { getUser } = require('../controllers/users')
+const { getUser, updateUser } = require('../controllers/users')
 const router = express.Router()
-
 
 router.route('/')
     .get(getUser)
+    .patch(updateUser)
 
 module.exports = router
