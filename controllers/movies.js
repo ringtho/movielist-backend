6de381416fd0f68b4dab7167ffb149d44b07de52
Smith Ringtho <sringtho@gmail.com>
2023-10-12 21:00:27 +0300

    Feat: Added update movie

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require("http-status-codes")
 const Movie = require("../models/movies")
-const { NotFoundError } = require("../errors")
+const { NotFoundError, BadRequestError } = require("../errors")
 
 const getAllMovies = async (req, res) => {
     const { id } = req.user
@@ -21,12 +21,31 @@ const getSingleMovie = async (req, res) => {
   res.status(StatusCodes.OK).json({ movie, success: true })
 }
 
-const updateMovie = (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Update movie' })
+const updateMovie = async (req, res) => {
+  const { user: { id: userId }, params: { id }} = req
+  if (JSON.stringify(req.body) === '{}') {
+    throw new BadRequestError('Please provide a field to update')
+  }
+  const [rowCount] = await Movie.update(
+    req.body, 
+    { where: { id, createdBy: userId}})
+  if(rowCount === 0) {
+    throw new NotFoundError(`Movie with id ${id} not found`)
+  }
+  const movie = await Movie.findOne({ where: { id } })
+  res.status(StatusCodes.OK).json({ movie, success: true })
 }
 
-const deleteMovie = (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Delete movie' })
+const deleteMovie = async (req, res) => {
+  const { user: { id: userId }, params: { id }} = req
+  const movie = await Movie.destroy({ where: { id , createdBy: userId }})
+  if (!movie) {
+    throw new NotFoundError(`Movie with id ${id} not found`)
+  }
+  res.status(StatusCodes.OK).json({ 
+    msg: `Successfully deleted movie with id ${id}`, 
+    success: true 
+  })
 }
 
 module.exports = {
