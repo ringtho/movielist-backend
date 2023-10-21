af2aafecab2c6d886793b5982dad7a5cbd93449c
Smith Ringtho <sringtho@gmail.com>
2023-10-21 09:00:30 +0300

    Refactor: Removed the security packages causing the app to break

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ const errorHandler = require('./middleware/error-handler')
 const authMiddleware = require('./middleware/authentication')
 
 // Security packages (middlewares)
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  })
-)
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+//   })
+// )
 
 app.use(cors())
-app.use(xss())
-app.use(helmet())
+// app.use(xss())
+// app.use(helmet())
 app.use(express.json())
 
 // Makes the images folder public
