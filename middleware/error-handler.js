38c7ca805d2162e7f8570130ae5ba2fe68c1ae87
Smith Ringtho <sringtho@gmail.com>
2023-10-25 08:34:30 +0300

    Refactor: Updated error handler to catch multer error and syntax errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,36 +3,44 @@ const { CustomApiError } = require("../errors")
 
 
 const errorHandler = (err, req, res, next) => {
-
   if (err instanceof CustomApiError) {
-    return res.status(err.statusCode).json({ error: err.message, success: false })
-  }
-
+    return res
+      .status(err.statusCode)
+      .json({ error: err.message, success: false })
+  } 
   if (err.name === 'SequelizeUniqueConstraintError') {
     return res
       .status(StatusCodes.CONFLICT)
-      .json({ error: `${err.fields.email} already exists!`, success: false})
+      .json({ error: `${err.fields.email} already exists!`, success: false })
   }
-
-  if (
-    err?.errors[0]?.type === 'Validation error' ||
-    err?.errors[0]?.type === 'notNull Violation'
-  ) {
+  if (err.name === 'MulterError') {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ 
+        error: 'Please provide an image file with a key "thumbnail" during image upload', 
+        success: false 
+      })
+  }
+  if (err.name === 'SequelizeValidationError') {
     const name = err?.errors[0].path
     return res.status(StatusCodes.BAD_REQUEST).json({
       error: `Please provide ${
         name.startsWith('e' || 'o' || 'i') ? 'an' : 'a'
       } ${name}`,
-      success: false
+      success: false,
     })
   }
-
-  return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({
+  if (err.name === 'SyntaxError') {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({
+        error: "Please check your json body for syntax errors", 
+        success: false })
+  }
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: 'Something went wrong, Please try again later',
       success: false,
-  })
+    })
 }
 
 module.exports = errorHandler
